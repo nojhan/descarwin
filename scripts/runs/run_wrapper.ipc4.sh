190a97136c3b4ccf788c86fa6e5a809abd8ccadc
Pierre Savéant <pierre.saveant@thalesgroup.com>
2010-11-02 15:19:54 +0000

    IPC4 running scripts

---
+++ scripts/runs/run_wrapper.ipc4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# no core dump
+ulimit -c 1
+
+domain=$1
+instance=$2
+options=$3
+res=$4
+
+r=$SGE_TASK_ID
+#r=2
+s=$((`date +%s`/r))
+
+i=`basename $instance .PDDL`
+
+echo "Run dae: $i, run $r, out $res" 1>&2
+
+cmd="./dae --max-seconds=10799 --domain=$domain --instance=$instance --seed=$s --status=$res/status/dae_status_${i}_r$r --sequential=$3 --plan-file=$res/plans/dae_plan_${i}_r$r"
+
+
+echo "  $cmd" 1>&2
+
+$cmd
+
--- scripts/runs/sge_submit_arrays_ipc4.sh
+++ scripts/runs/sge_submit_arrays_ipc4.sh
@@ -19,57 +19,107 @@ function sge_submit_array_ipc()
     # directory where to put everything
     res=$4
 
-    # create directories if they do not exists
-    mkdir -p $res
-    mkdir -p $res/data
-    mkdir -p $res/logs
-    mkdir -p $res/status
-    mkdir -p $res/plans
-    mkdir -p $res/gens
+ 
 
     # list the available domain files in the sub-tree of this directory
-    # default: do not consider ADL
-    # domains=`find $base_rep -wholename "*/SimpleTime/*.pddl" -print | grep -v "SimpleTime.*[0-9]" | grep -v adl`
-    #domains=`find $base_rep -wholename "*/Strips/*.pddl" -print | grep -v adl`
-    # domains=`find $base_rep -wholename "*/Strips/*.pddl" -print | grep -v adl ; find $base_rep -wholename "*/SimpleTime/*.pddl" -print | grep -v "SimpleTime.*[0-9]" | grep -v adl`
 
-    domains=""
-    if [ $options == 0 ] ; then
-	domains=`find $base_rep -wholename "./DOMAINS/AIRPORT/TEMPORAL*/*_DOMAIN.PDDL"`
-    else
-	domains=`find $base_rep -wholename "./DOMAINS/AIRPORT/NONTEMPORAL*/STRIPS/*_DOMAIN.PDDL";find $base_rep -wholename "./DOMAINS/PSR/STRIPS/*_DOMAIN.PDDL"; grep -v adl;find $base_rep -wholename "./DOMAINS/PROMELA/STRIPS/*_DOMAIN.PDDL"`
-    fi
+    domains=`find $base_rep -wholename "*_DOMAIN.PDDL"`
     
     for domain in $domains; do
 
         cptr=0
-	
-	instances=`find $base_rep -wholename "./DOMAINS/AIRPORT/TEMPORAL*/*" | grep -v DOMAIN`
-	for instance in $instances; do
+	pattern=`echo $domain | sed s/_DOMAIN/\*/`
+	instance=`ls $pattern | grep -v _DOMAIN`
 
-	    if test -r $instance && test -r $domain; then
+	if test -r $instance && test -r $domain; then
 
-            # parent directory
-		d=`basename $domain .pddl`
-echo $d
-echo $i
+            # create directory tree
+	    respath=$res/`dirname $domain | sed "s/\/tools\/pddl\/ipc\///"`
+
+            # create directories if they do not exists
+	    mkdir -p $respath
+	    mkdir -p $respath/data
+	    mkdir -p $respath/logs
+	    mkdir -p $respath/status
+	    mkdir -p $respath/plans
+	    mkdir -p $respath/gens
+
             # submit an array of *runs jobs
             # additional arguments are in $options
-		cmd1="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N dae_${d}_${i}      -cwd -o $res/data/ -e $res/logs/ -t 1-$runs -S /bin/bash ./run_wrapper.sh $domain $instance $options $res"
-		cmd2="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N jack_${d}_${i} -cwd -o $res/data/ -e $res/logs/ -t 1-$runs -S /bin/bash ./run_wrapper_jack.sh $domain $instance $options $res"
+		cmd1="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N dae      -cwd -o $respath/data/ -e $respath/logs/ -t 1-$runs -S /bin/bash ./run_wrapper.ipc4.sh $domain $instance $options $respath"
+		cmd2="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N jack -cwd -o $respath/data/ -e $respath/logs/ -t 1-$runs -S /bin/bash ./run_wrapper_jack.ipc4.sh $domain $instance $options $respath"
 		
 		echo $cmd1
 		echo $cmd2
-            #./run_wrapper.sh $domain $instance $options
-            #./run_wrapper_jack.sh $domain $instance $options
-#		$cmd1
-#		$cmd2
+#            ./run_wrapper.ipc4.sh $domain $instance $options $respath
+#            ./run_wrapper_jack.ipc4.sh $domain $instance $options $respath
+		$cmd1
+		$cmd2
 
 		cptr=$((cptr+1))
 	    else
 		echo "ERROR, instance or domain files cannot be open: $instance"
 	    fi
-	done # i
+    done # domain
+
+    echo "$((cptr*runs)) runs submitted"
+}
+
+function sge_submit_array_ipc_single_domain()
+{
+
+    # directory where to search PDDL domains and instance files 
+    base_rep=$1
+
+    # additional options to give to the solver (e.g. --sequential)
+    options=$2
+
+    # nb of differents runs for each instance
+    runs=$3
+
+    # directory where to put everything
+    res=$4
+
+    # list the available domain files in the sub-tree of this directory
+
+    domain=$base_rep/DOMAIN.PDDL
+    instances=`ls $base_rep/P*.PDDL`
+
+
+
+    for instance in $instances; do
+
+        cptr=0
+
+	if test -r $instance && test -r $domain; then
+
+            # create directory tree
+	    respath=$res/`dirname $domain | sed "s/\/tools\/pddl\/ipc\///"`
+
+            # create directories if they do not exists
+	    mkdir -p $respath
+	    mkdir -p $respath/data
+	    mkdir -p $respath/logs
+	    mkdir -p $respath/status
+	    mkdir -p $respath/plans
+	    mkdir -p $respath/gens
+
+            # submit an array of *runs jobs
+            # additional arguments are in $options
+		cmd1="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N dae      -cwd -o $respath/data/ -e $respath/logs/ -t 1-$runs -S /bin/bash ./run_wrapper.ipc4.sh $domain $instance $options $respath"
+		cmd2="qsub -l h_rt=00:30:00 -q all.q@@ls -b y -N jack -cwd -o $respath/data/ -e $respath/logs/ -t 1-$runs -S /bin/bash ./run_wrapper_jack.ipc4.sh $domain $instance $options $respath"
+		
+		echo $cmd1
+		echo $cmd2
+#            ./run_wrapper.ipc4.sh $domain $instance $options $respath
+#            ./run_wrapper_jack.ipc4.sh $domain $instance $options $respath
+		$cmd1
+		$cmd2
+
+		cptr=$((cptr+1))
+	    else
+		echo "ERROR, instance or domain files cannot be open: $instance"
+	    fi
     done # domain
 
     echo "$((cptr*runs)) runs submitted"
@@ -89,7 +139,10 @@ runs=11
 # AIRPORT
 # domain=/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/NONTEMPORAL/STRIPS/P01_DOMAIN.PDDL to P50_DOMAIN.PDDL
 # instance=/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/NONTEMPORAL/STRIPS/P01_*.PDDL to P50_*.PDDL
-#sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/NONTEMPORAL/STRIPS" 1 $runs "seq-sat"
+#                   sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/NONTEMPORAL/STRIPS" 1 $runs "seq-sat"
+
+
+
 
 # domain=/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/TEMPORAL/STRIPS_TEMPORAL/P01_DOMAIN.PDDL to P50_DOMAIN.PDDL
 # instance=/tools/pddl/ipc/IPC4/DOMAINS/AIRPORT/TEMPORAL/STRIPS_TEMPORAL/P01_*.PDDL to P50_*.PDDL
@@ -107,15 +160,15 @@ runs=11
 # PIPESWORLD
 # domain=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/NOTANKAGE_NONTEMPORAL/STRIPS/DOMAIN.PDDL
 # instance=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/NOTANKAGE_NONTEMPORAL/STRIPS/P01_*.PDDL to P50_*.PDDL
-#sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/NOTANKAGE_NONTEMPORAL/STRIPS" 1 $runs "seq-sat"
+sge_submit_array_ipc_single_domain "/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/NOTANKAGE_NONTEMPORAL/STRIPS" 1 $runs "seq-sat"
 
 # domain=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_NONTEMPORAL/STRIPS/DOMAIN.PDDL
 # instance=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_NONTEMPORAL/STRIPS/P01_*.PDDL to P50_*.PDDL
 #sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_NONTEMPORAL/STRIPS" 1 $runs "seq-sat"
 
-# domain=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS/DOMAIN.PDDL
-# instance=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS/P01_*.PDDL to P50_*.PDDL
-#sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS" 0 $runs "tempo-sat"
+# domain=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS_TEMPORAL/DOMAIN.PDDL
+# instance=/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS_TEMPORAL/P01_*.PDDL to P50_*.PDDL
+#sge_submit_array_ipc "/tools/pddl/ipc/IPC4/DOMAINS/PIPESWORLD/TANKAGE_TEMPORAL/STRIPS_TEMPORAL" 0 $runs "tempo-sat"
 
 # PROMELA
 # domain=/tools/pddl/ipc/IPC4/DOMAINS/PROMELA/OPTICAL_TELEGRAPH/STRIPS/P01_DOMAIN*.PDDL to P14_DOMAIN*.PDDL
