d50197110d38cc29683282767bc349926a725474
Pierre Savéant <pierre.saveant@thalesgroup.com>
2011-01-03 16:17:01 +0000

    incremental bmax strategy updated

--- dae/dae.cpp
+++ dae/dae.cpp
@@ -319,43 +319,34 @@ int main ( int argc, char* argv[] )
     // randomly generate the population with the init operator
     eoPop<daex::Decomposition> pop = eoPop<daex::Decomposition>( pop_size, init );
 
-    eo::log << eo::progress << "OK" << std::endl;
+    //    eo::log << eo::progress << "OK" << std::endl;
     
-    // search for the minimum b_max that needed to solve most of the population 
-
     unsigned int b_max_in=1, b_max_last, goodguys, popsize = pop.size();
 
-    // If we want the incremental strategy
-    if (b_max_fixed == 0) {
-        eo::log << eo::progress << "Apply an incremental computation strategy to fix bmax:" << std::endl;
-        do {
-            goodguys = 0;
-            b_max_last = static_cast<unsigned int>(std::floor (b_max_in * b_max_last_weight));
+    // Incremental strategy to fix bmax
+    if( b_max_fixed == 0 ) {
+      eo::log << eo::progress << "Apply an incremental computation strategy to fix bmax:" << std::endl;
 
-            daeYahspEval eval_yahsp (init.l_max (), b_max_in, b_max_last, fitness_weight, fitness_penalty, is_sequential);
-            eoPopLoopEval < daex::Decomposition > eval_y (eval_yahsp);
-            eval_y (pop, pop);
+      while ((((double)goodguys/(double)popsize) < b_max_ratio) && (b_max_in < b_max_init)) {
+	goodguys=0;
+	b_max_last=static_cast<unsigned int>( std::floor( b_max_in * b_max_last_weight ) );
 
-            for (size_t i = 0; i < popsize; ++i) {
-                if (pop[i].fitness ().is_feasible ())
-                  goodguys++;
-                else
-                  pop[i].invalidate ();
-            }
-
-            eo::log << eo::logging << "b_max_in= " << b_max_in << ", current feasible ratio= " << ((double) goodguys / (double) popsize) << std::endl;
-            b_max_in = (unsigned int) ceil (b_max_in * b_max_increase_coef);
-        }
-
-        while ((((double) goodguys / (double) popsize) < b_max_ratio) && (b_max_in < b_max_init));
-        //      while (goodguys == 0);
-
-    } else { // if b_max_in != 0
-        b_max_in = b_max_fixed;
-        b_max_last = static_cast < unsigned int >(std::floor (b_max_in * b_max_last_weight));
-        eo::log << eo::progress << "No evaluation of b_max, fixed to...";
-        eo::log.flush ();
-    }
+	daeYahspEval eval_yahsp( init.l_max(), b_max_in, b_max_last, fitness_weight, fitness_penalty, is_sequential );
+	eoPopLoopEval<daex::Decomposition> eval_y( eval_yahsp );
+	eval_y( pop, pop );
+	for (size_t i = 0; i < popsize; ++i)
+	  { // unfeasible individuals are invalidated in order to be re-evaluated with a larger bmax at the next iteration but we keep the good guys.
+	    if (pop[i].fitness().is_feasible()) goodguys++;
+	    else pop[i].invalidate();
+	  }
+	eo::log << eo::logging << "b_max_in= "   << b_max_in << ", current feasible ratio= " <<  ((double)goodguys/(double)popsize) << std::endl;
+	b_max_fixed = b_max_in;
+	b_max_in = (unsigned int)ceil(b_max_in*b_max_increase_coef);
+      }
+      //      while (goodguys == 0);
+    } 
+    b_max_in = b_max_fixed;
+    b_max_last = static_cast<unsigned int>( std::floor( b_max_in * b_max_last_weight ) );
 
     daeYahspEval eval_yahsp( init.l_max(), b_max_in, b_max_last, fitness_weight, fitness_penalty, is_sequential );
     eoPopLoopEval<daex::Decomposition> eval_y( eval_yahsp );
