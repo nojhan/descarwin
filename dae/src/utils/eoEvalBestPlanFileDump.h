f4d755418150b55c574853369be54c9ecc8f76fb
Johann Dreo <johann.dreo@thalesgroup.com>
2011-01-07 14:46:40 +0000

    BUGFIX: on x86-64, when called inside a ofstream::open, the filename function call returns a corrupted string, thus: back to a inline code

--- dae/src/utils/eoEvalBestPlanFileDump.h
+++ dae/src/utils/eoEvalBestPlanFileDump.h
@@ -2,6 +2,8 @@
 #ifndef eoEvalBestPlanFileDump_H
 #define eoEvalestPlanFileDump_H
 
+#include <fstream>
+
 #include <eoEvalFunc.h>
 #include <utils/eoParam.h>
 
@@ -22,11 +24,11 @@ public:
             eoEvalFunc<Decomposition>& func, 
             std::string afilename, 
             Decomposition::Fitness worst_fitness,
-            bool overwrite = false,
+            bool single_file = false,
             std::string sep = ".",
             unsigned int file_count = 0)
         : _func(func), _filename(afilename),_best_fitness(worst_fitness), _sep(sep),
-          _overwrite(overwrite), _file_count(file_count)
+          _single_file(single_file), _file_count(file_count)
     {
         /*
         // to test if the file can be opened, we use the "app" mode, 
@@ -57,8 +59,18 @@ public:
 
                     _best_fitness = eo.fitness();
 
-                    // explicitely erase the file before writing in it
-                    _of.open( filename(), std::ios_base::out | std::ios_base::trunc );
+                    if( _single_file ) {
+                         // explicitely erase the file before writing in it
+                         _of.open( _filename.c_str(), std::ios_base::out | std::ios_base::trunc );
+
+                     } else {
+                        std::ostringstream afilename;
+                        afilename << _filename << _sep << _file_count;
+                        _of.open( afilename.str().c_str(), std::ios_base::out | std::ios_base::trunc );
+                     }
+
+                    // BUG 
+                    //_of.open( this->filename(), std::ios_base::out | std::ios_base::trunc );
 
 #ifndef NDEBUG
                     if ( !_of.is_open() ) {
@@ -89,23 +101,25 @@ public:
     unsigned int file_count() { return _file_count; }
 
 protected:
+    /* TODO on x86-64, when called inside a ofstream::open, this functional call returns a corrupted string
     const char * filename()
     {
-        if( _overwrite ) {
+        if( _single_file ) {
             return _filename.c_str();
 
          } else {
-            std::ostringstream filename;
-            filename << _filename << _sep << _file_count;
-            return filename.str().c_str();
+            std::ostringstream afilename;
+            afilename << _filename << _sep << _file_count;
+            return afilename.str().c_str();
          }
     }
+    */
 
     eoEvalFunc<Decomposition>& _func;
     std::string _filename;
     std::ofstream _of;
     Decomposition::Fitness _best_fitness;
-    bool _overwrite;
+    bool _single_file;
     unsigned int _file_count;
     std::string _sep;
 };
