38473cb95d3e4f7532b22751985650d09505145e
Johann Dreo <johann.dreo@thalesgroup.com>
2011-01-13 12:58:43 +0000

    suppress useless errors messages

--- scripts/runs/copy_lasts_plans.sh
+++ scripts/runs/copy_lasts_plans.sh
@@ -2,8 +2,11 @@ domains=`ls -1|awk -F _p[0-9] '{print $1}'|uniq `
 
 for d in $domains ; do
     for i in {0..2}{0..9} ; do 
-         for r in {1..11} ; do 
-             plan=`ls -1 -v ${d}_p${i}_r${r}.* |tail -n 1`
+        if [ $i == 00 ] ; then
+            continue
+        fi
+        for r in {1..11} ; do 
+             plan=`ls -1 -v ${d}_p${i}_r${r}.* |tail -n 1 2>/dev/null`
              if [ $plan != "" ] ; then
                 to=`echo ${plan}|cut -d '.' -f 1`.soln
                 echo $to
