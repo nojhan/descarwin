48a107957b3e40a1ab0f36d8c35f0e3f7fc44928
Vincent Vidal <vincent.vidal@onera.fr>
2011-02-04 10:10:04 +0000

    modified critical sections again.

--- dae/src/utils/evalBestPlanDump.cpp
+++ dae/src/utils/evalBestPlanDump.cpp
@@ -6,8 +6,11 @@ namespace daex {
 void evalBestMakespanPlanDump::call( Decomposition & eo )
 {
     if( eo.plan().makespan() < _best ) {
-        _best = eo.plan().makespan();
-        dump( eo );
+#pragma omp critical
+        if( eo.plan().makespan() < _best ) {
+            _best = eo.plan().makespan();
+            dump( eo );
+        }
     } // if better
 }
 
@@ -16,8 +19,11 @@ void evalBestFitnessPlanDump::call( Decomposition & eo )
     // Note: in EO, maximizing by default, later overloaded for minimizing
   // OMP DIRTY - first comparison is unprotected, in order to be non-blocking
     if( eo.fitness() > _best ) {
-        _best = eo.fitness();
-        dump( eo );
+#pragma omp critical
+        if( eo.fitness() > _best ) {
+            _best = eo.fitness();
+            dump( eo );
+        }
     } // if better
 }
 
