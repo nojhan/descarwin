3b1165f77b9bb2f23720f62bbe4d85d37318140d
Caner Candan <caner@candan.fr>
2011-02-12 11:51:20 +0000

    * last updates on the script GECCO2011.py

--- scripts/GECCO2011.py
+++ scripts/GECCO2011.py
@@ -2,6 +2,7 @@
 
 import optparse, logging, sys, os
 from datetime import datetime
+import time
 import common
 
 logger = logging.getLogger("GECCO2011")
@@ -43,40 +44,42 @@ parser.add_option('-e', '--execute', action='store_true', default=False, help='e
 parser.add_option('-D', '--dynamic', action='store_true', default=False, help='use the dynamic scheduler in openmp')
 parser.add_option('-p', '--plot', action='store_true', default=False, help='plot data')
 parser.add_option('-N', '--nruns', type='int', default=11, help='give here a number of runs')
+
 topic = str(datetime.today())
 for char in [' ', ':', '-', '.']: topic = topic.replace(char, '_')
 parser.add_option('-t', '--topic', default=topic, help='give here a topic name used to create the folder')
+
+parser.add_option('-C', '--cores', action='store_true', default=False, help='enable results on processus variations')
+parser.add_option('-S', '--sizes', action='store_true', default=False, help='enable results on population sizes variations')
+
 options = common.parser(parser)
 
-def main():
+if options.plot:
+    import pylab
 
-    resdir = "%s/Res" % options.topic
-    timedir = "%s/Time" % options.topic
+def do_proc(resdir, timedir):
+    """
+    EXPS on IPC6_SEQ_ELEVATORS_12 & IPC6_TEMPO_OPENSTACKS_17
+    steadyState=50
+    1) popsize=48 & runmax=1 & maxseconds=0
+    2) RESTART case: popsize=96 & runmax=0 & maxseconds=1799
+    foreach nthreads: 1, 24, 48
+    repeat 11 times
+    """
 
-    # create needed directories
-    if options.execute:
-        for d in [resdir, timedir]:
-            try:
-                os.makedirs(d)
-            except OSError:
-                pass
+    if not options.cores: return
 
-
-    # EXPS on IPC6_SEQ_ELEVATORS_12 & IPC6_TEMPO_OPENSTACKS_17
-    # steadyState=50
-    # 1) popsize=48 & runmax=1 & maxseconds=0
-    # 2) RESTART case: popsize=96 & runmax=0 & maxseconds=1799
-    # foreach nthreads: 1, 24, 48
-    # repeat 11 times
-
-    for field, popsize, runmax, maxseconds in [("PROC", 48, 1, 0),
-                                               ("RESTART_PROC", 96, 0, 1799)]:
+    for field, popsize, runmax, maxseconds in [
+        ("PROC", 48, 1, 0),
+        #("RESTART_PROC", 96, 0, 1799)
+        ]:
         for name, domain, instance in SAMPLES:
+            local_logger = logging.getLogger("GECCO2011.PROC.%s" % name)
             plotdata = []
             for num in range(1, options.nruns+1):
                 subdata = []
                 for nthreads in [1, 24, 48]:
-                    field_name = "%s_%s_%d" % (field, "DYNAMIC" if options.dynamic else "STATIC", popsize)
+                    field_name = "%s_%s_%d" % (field, "DYNAMIC" if options.dynamic else "STATIC", nthreads)
                     time_filename = PATTERN_TIME_FILENAME % {"TIMEDIR": timedir, "NAME": name, "FIELD": field_name, "NUM": num}
                     res_filename = PATTERN_RES_FILENAME % {"RESDIR": resdir, "NAME": name, "FIELD": field_name, "NUM": num}
                     plan_filename = PATTERN_PLAN_FILENAME % {"RESDIR": resdir, "NAME": name, "FIELD": field_name, "NUM": num}
@@ -94,33 +97,45 @@ def main():
                                          "RES_FILENAME": res_filename,
                                          "PLAN_FILENAME": plan_filename,
                                          }
-                    logger.debug(cmd)
+                    local_logger.debug(cmd)
                     if options.execute:
                         os.system( cmd )
                     if options.plot:
                         try:
-                            subdata.append(open(time_filename).readlines()[4].split()[-1])
+                            f = open(time_filename).readlines()
+                            t1 = float(f[1].split()[-1])
+                            tp = f[4].split()[-1].split(':')
+                            tp = float(int(tp[0]) * 60 + float(tp[1]))
+                            subdata.append([t1, tp, t1 / tp])
                         except IOError:
                             pass
 
                 if options.plot:
-                    #logger.info(subdata)
                     if len(subdata):
                         plotdata.append(subdata)
 
-            logger.info(plotdata)
+            if options.plot:
+                local_logger.info(plotdata)
+                pylab.boxplot( plotdata )
 
+def do_pop(resdir, timedir):
+    """
+    EXPS on IPC6_SEQ_ELEVATORS_12 & IPC6_TEMPO_OPENSTACKS_17
+    nthreads=48 & steadyState=50
+    1) runmax=1 & maxseconds=0
+    2) RESTART case: runmax=0 & maxseconds=1799
+    foreach popSize: 1, 24, 48, 72, 96
+    repeat 11 times
+    """
 
-    # EXPS on IPC6_SEQ_ELEVATORS_12 & IPC6_TEMPO_OPENSTACKS_17
-    # nthreads=48 & steadyState=50
-    # 1) runmax=1 & maxseconds=0
-    # 2) RESTART case: runmax=0 & maxseconds=1799
-    # foreach popSize: 1, 24, 48, 72, 96
-    # repeat 11 times
+    if not options.sizes: return
 
-    for field, runmax, maxseconds in [("POP", 1, 0),
-                                      ("RESTART_POP", 0, 1799)]:
+    for field, runmax, maxseconds in [
+        ("POP", 1, 0),
+        #("RESTART_POP", 0, 1799)
+        ]:
         for name, domain, instance in SAMPLES:
+            local_logger = logging.getLogger("GECCO2011.POP.%s" % name)
             plotdata = []
             for num in range(1, options.nruns+1):
                 subdata = []
@@ -143,21 +158,60 @@ def main():
                                          "RES_FILENAME": res_filename,
                                          "PLAN_FILENAME": plan_filename,
                                          }
-                    logger.debug(cmd)
+                    local_logger.debug(cmd)
                     if options.execute:
                         os.system( cmd )
                     if options.plot:
                         try:
-                            subdata.append(open(time_filename).readlines()[4].split()[-1])
+                            f = open(time_filename).readlines()
+                            t1 = float(f[1].split()[-1])
+                            tp = f[4].split()[-1].split(':')
+                            tp = float(int(tp[0]) * 60 + float(tp[1]))
+                            #subdata.append([t1, tp, t1 / tp])
+                            subdata.append(t1 / tp)
                         except IOError:
                             pass
 
                 if options.plot:
-                    #logger.info(subdata)
                     if len(subdata):
                         plotdata.append(subdata)
 
-            logger.info(plotdata)
+            if options.plot:
+                local_logger.info( plotdata )
+
+                newdata = []
+                for i in range(0,5):
+                    newdata.append([0] * 9)
+
+                for i in range(0,9):
+                    for j in range(0,5):
+                        newdata[j][i] = plotdata[i][j]
+
+                print "NEWDATA"
+                print newdata
+
+                pylab.boxplot( newdata )
+                pylab.xlabel('population sizes')
+                pylab.ylabel('speedup with 48 cores')
+                pylab.savefig( name + '_output.pdf', format='pdf' )
+                pylab.savefig( name + '_output.png', format='png' )
+                pylab.cla()
+                pylab.clf()
+
+def main():
+    resdir = "%s/Res" % options.topic
+    timedir = "%s/Time" % options.topic
+
+    # create needed directories
+    if options.execute:
+        for d in [resdir, timedir]:
+            try:
+                os.makedirs(d)
+            except OSError:
+                pass
+
+    do_proc(resdir, timedir)
+    do_pop(resdir, timedir)
 
 # when executed, just run main():
 if __name__ == '__main__':
