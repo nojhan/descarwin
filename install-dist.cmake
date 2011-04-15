ff594174b19ec8bd82e1b0a0ce9b1319a51f622f
Caner Candan <caner@candan.fr>
2011-04-15 17:06:31 +0000

    * update cmake config files and + added README

--- install-dist.cmake
+++ install-dist.cmake
@@ -1,7 +1,15 @@
 
 # Variables to set
 
+#  directory where EO has been installed
+SET(EO_DIR "<<PATH_TO_EO>>" CACHE PATH "EO directory" FORCE)
+
 # directory where GDSL has been installed
 # these variables must be only defined if you are using a customized installation of GDSL
 #SET(GDSL_INCLUDE_DIR "<<PATH_TO_GDSL_INCLUDE_DIR>>")
 #SET(GDSL_LIBRARY "<<PATH_TO_GDSL_LIBRARY>>")
+
+# automagically set parameters, do not edit
+
+SET(EO_SRC_DIR "${EO_DIR}/src" CACHE PATH "EO source directory" FORCE)
+SET(EO_BIN_DIR "${EO_DIR}/release" CACHE PATH "EO binary directory" FORCE)
