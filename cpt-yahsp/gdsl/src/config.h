178a76f6c23089a08aac1967d5889aafdcbebcc3
Vincent Vidal <vincent.vidal@onera.fr>
2011-07-01 09:00:48 +0000

    rép gdsl dans cpt-yahsp oublié...

---
+++ cpt-yahsp/gdsl/src/config.h
@@ -0,0 +1,4 @@
+#define VERSION "1.4"
+#define HAVE_LINUX 1
+#define NDEBUG 1
+
---
+++ cpt-yahsp/gdsl/src/mainpage.h
@@ -0,0 +1,25 @@
+/** @mainpage gdsl
+ *
+ * @section intro Introduction
+ *
+ * This is the %gdsl (Release 1.4) documentation.
+ *
+ * @section about About
+ *
+ *  The Generic Data Structures Library (GDSL) is a collection of routines for
+ *  generic data structures manipulation. It is a portable and re-entrant 
+ *  library fully written from scratch in pure ANSI C. It is designed to offer
+ *  for C programmers common data structures with powerful algorithms, and 
+ *  hidden implementation. Available structures are lists, queues, stacks, hash
+ *  tables, binary trees, binary search trees, red-black trees, 2D arrays,
+ *  permutations and heaps.
+ *
+ * @subsection authors Authors
+ *
+ * Nicolas Darnis <ndarnis@free.fr> <BR>
+ * 
+ * @subsection cdp Project Manager
+ *
+ * 
+ * Nicolas Darnis <ndarnis@free.fr> <BR>
+ */
