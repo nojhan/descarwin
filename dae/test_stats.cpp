f6329b35e41d9eca7325d5e5263c760d5851e024
Benjamin Bouvier <public@benj.me>
2012-05-14 13:02:06 +0000

    Bugfix: compilation in debug mode, use normal fitness instead of eoDualFitness.

--- dae/test_stats.cpp
+++ dae/test_stats.cpp
@@ -6,7 +6,7 @@
 
 #include <eo>
 #include <do/make_pop.h>
-#include <utils/eoFeasibleRatioStat.h>
+#include "utils/FeasibleStat.h"
 
 #include "daex.h"
 #include "evaluation/yahsp.h"
@@ -54,7 +54,7 @@ int main ( int argc, char* argv[] )
     //std::clog << "# Sequential: " << is_sequential << std::endl;
     
     eoState state;
-    daex::Init init( pddl.chronoPartitionAtom(), l_max_init_coef, l_min );
+    daex::Init<> init( pddl.chronoPartitionAtom(), l_max_init_coef, l_min );
     eoPop<daex::Decomposition> pop = do_make_pop( parser, state, init );
     make_help( parser );
 
@@ -62,7 +62,7 @@ int main ( int argc, char* argv[] )
     // permits to compute the b_max
     // (b_max = 10 000 by default)
     // uses the same value for b_max_in and b_max_last
-    daeYahspEval eval_yahsp( init.l_max(), b_max_in, static_cast<unsigned int>(b_max_in*b_max_last_weight), fitness_weight, fitness_penalty );
+    daeYahspEval<> eval_yahsp( init.l_max(), b_max_in, static_cast<unsigned int>(b_max_in*b_max_last_weight), fitness_weight, fitness_penalty );
 
     // start the eval on the first random pop
     eoPopLoopEval<daex::Decomposition> eval( eval_yahsp );
@@ -83,8 +83,9 @@ int main ( int argc, char* argv[] )
 
     eoBestFitnessStat<daex::Decomposition> best_stat;
     eoNthElementFitnessStat<daex::Decomposition> median_stat( pop.size() / 2, "Median" ); 
-    eoInterquartileRangeStat<daex::Decomposition> iqr_stat( std::make_pair(0.0,false) );
-    eoFeasibleRatioStat<daex::Decomposition> feasible_stat( "F.Ratio" );
+    //eoInterquartileRangeStat<daex::Decomposition> iqr_stat( std::make_pair(0.0,false) );
+    eoInterquartileRangeStat<daex::Decomposition> iqr_stat( 0.0 );
+    daex::FeasibleRatioStat<daex::Decomposition> feasible_stat( "F.Ratio" );
 
 
     best_stat(pop);
