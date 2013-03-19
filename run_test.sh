b4b91ce8697ef091363ba5034c4da8a15df963f9
Johann Dreo <johann.dreo@thalesgroup.com>
2013-03-19 22:18:13 +0000

    bugfix run_test.sh fix output naming

--- run_test.sh
+++ run_test.sh
@@ -2,10 +2,17 @@
   domain="./scripts/runs/ipc6_tempo-sat_elevators-strips_p01-domain.pddl"
 instance="./scripts/runs/ipc6_tempo-sat_elevators-strips_p01.pddl"
 
-res=$(mktemp --directory)
+d=$(basename $domain .pddl)
+i=$(basename $instance .pddl)
+plan="./dae_test_${d}_${i}_s0.plan"
 
-cmd="./debug/dae/dae --verbose=debug --max-seconds=1799 --domain=$domain --instance=$instance --seed=0 --status=$res/status/dae_status.${domain}_${instance}_s0 --plan-file=$res/plans/dae_plan_${domain}_${instance}_r0 --runs-max=1"
+cmd="./debug/dae/dae --domain=${domain} --instance=${instance} --seed=0 --verbose=debug
+--status=./dae_test_${d}_${i}_s0.status --plan-file=${plan}
+--max-seconds=1799 --runs-max=1 --gen-max=3 --bmax-fixed=100 $*"
 
 echo $cmd
 $cmd
+
+echo "PLAN:"
+cat $plan
 
