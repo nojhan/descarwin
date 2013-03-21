ac0e4a9e6d99bc3abd7e97477ab7ee93c42e82bf
Johann Dreo <johann.dreo@thalesgroup.com>
2013-03-21 11:24:37 +0000

    Sort build scripts by name size

--- run_build_all.sh
+++ run_build_all.sh
@@ -6,16 +6,22 @@ RAZ="\\033[0;39m"
 
 build_scripts=$(ls -1 --color=none build_*)
 
+bs=()
+nb=0
 max=0
 for b in $build_scripts; do
+    nb=$((nb+1))
+    bs=(${bs[@]} $b)
     if [[ ${#b} > $max ]]; then
         max=${#b}
     fi
 done
 
-for b in $build_scripts; do
-    printf "%${max}s ... " "$b"
-    ./$b 1>/dev/null 2>/dev/null
+items=$( for i in $(seq 0 $((nb-1)) ); do printf "${#bs[i]}_$i\n"; done | sort | cut -d "_" -f 2 )
+
+for i in $items; do
+    printf "%${max}s ... " "${bs[$i]}"
+    ./${bs[$i]} 1>/dev/null 2>/dev/null
     if [[ $? == 0 ]]; then
         printf "${OK} OK${RAZ}\n"
     else
