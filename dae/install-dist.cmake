69fd0b466d9c46881ad8b0e613d4567ba0b0e1ec
Johann Dreo <johann.dreo@thalesgroup.com>
2013-04-09 13:08:04 +0000

    Simplify the build system to keep only meaningful variables in the cmake cache

--- dae/install-dist.cmake
+++ dae/install-dist.cmake
@@ -1,10 +1,5 @@
 
 #  directory where EO has been installed
-SET(PARADISEO_DIR "<<PATH_TO_PARADISEO>>" CACHE PATH "ParadisEO directory" FORCE)
-
-set(PARADISEO_BUILD_DIR "${PARADISEO_DIR}/release" CACHE PATH "ParadisEO build dir" FORCE)
-
-# directory where CPT-YAHSP has been installed
-SET(CPT_YAHSP_DIR "<<PATH_TO_CPT_YAHSP>>" CACHE PATH "CPT-YAHSP directory" FORCE)
-SET(CPT_YAHSP_BUILD_DIR "${CPT_YAHSP_DIR}/release" CACHE PATH "CPT-YAHSP library directory" FORCE)
+SET(PARADISEO_DIR "<<PATH_TO_PARADISEO>>" CACHE PATH "ParadisEO directory")
+set(PARADISEO_BUILD_DIR "${PARADISEO_DIR}/release" CACHE PATH "ParadisEO build dir")
 
--- install-dist.cmake
+++ install-dist.cmake
@@ -1,4 +1,1 @@
 
-# Variables to set
-SET(MPI_DIR "<< PATH_TO_OPEN_MPI_STATIC >>" CACHE PATH "OpenMPI directory" FORCE)
-
